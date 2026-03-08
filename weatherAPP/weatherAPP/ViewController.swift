88ea7037e8770d525a39392d7f56d25e28cf544b
maimona-alzaidi <malzaidi@innovationteam.com>
2026-03-08 13:12:01 +0300

    INIT PROJECT

---
+++ weatherAPP/weatherAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherAPP
+//
+//  Created by maimona alzaidi on 19/09/1447 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weatherAPP/weatherAPPTests/weatherAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAPPTests.swift
+//  weatherAPPTests
+//
+//  Created by maimona alzaidi on 19/09/1447 AH.
+//
+
+import Testing
+@testable import weatherAPP
+
+struct weatherAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
